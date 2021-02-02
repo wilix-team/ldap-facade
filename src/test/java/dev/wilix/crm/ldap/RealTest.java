cdbed66592294fd299d5a84409162fa005e2e607
okuneva.s <rheola@yandex.ru>
2021-02-02 16:50:46 +0300

    RealTest

---
+++ src/test/java/dev/wilix/crm/ldap/RealTest.java
@@ -0,0 +1,26 @@
+package dev.wilix.crm.ldap;
+
+import com.unboundid.ldap.sdk.BindResult;
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.ResultCode;
+import com.unboundid.util.LDAPTestUtils;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RealTest {
+    @Test
+    @Disabled("Для реального вызова")
+    public void disabled() throws LDAPException {
+
+        LDAPConnection connection = new LDAPConnection("localhost", 10636);
+
+        BindResult result = connection.bind("uid=svetlana.okuneva,ou=people,dc=wilix,dc=dev", "pwd");
+
+        LDAPTestUtils.assertResultCodeEquals(result, ResultCode.SUCCESS);
+
+        connection.close();
+    }
+}
