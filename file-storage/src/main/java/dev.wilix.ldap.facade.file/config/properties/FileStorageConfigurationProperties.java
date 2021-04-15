7845d56037dab6fdb17dac89d727af84ff2b5f50
Bondarenko Dmitry <dmitry.bondarenko@wilix.org>
2021-04-15 17:08:10 +0300

    Добавлена валидация, обновлены конфигурации application.yml.

--- file-storage/src/main/java/dev.wilix.ldap.facade.file/config/properties/FileStorageConfigurationProperties.java
+++ file-storage/src/main/java/dev.wilix.ldap.facade.file/config/properties/FileStorageConfigurationProperties.java
@@ -1,10 +1,15 @@
 package dev.wilix.ldap.facade.file.config.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotEmpty;
+
+@Validated
 @ConfigurationProperties(prefix = "storage.file")
 public class FileStorageConfigurationProperties {
 
+    @NotEmpty
     private String pathToFile;
 
     public String getPathToFile() {
