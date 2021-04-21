650231a08ed0aebe7e2730bba8438fc2872b4f91
Bondarenko Dmitry <dmitry.bondarenko@wilix.org>
2021-04-21 18:07:36 +0300

    Подправлены отступы.

--- server/src/main/java/dev/wilix/ldap/facade/server/processing/UserBindAndSearchRequestHandler.java
+++ server/src/main/java/dev/wilix/ldap/facade/server/processing/UserBindAndSearchRequestHandler.java
@@ -119,11 +119,8 @@ public class UserBindAndSearchRequestHandler extends AllOpNotSupportedRequestHan
 
         if (authentication == null || !authentication.isSuccess()) {
             return new LDAPMessage(messageID,
-                    new SearchResultDoneProtocolOp(
-                            ResultCode.INVALID_CREDENTIALS_INT_VALUE,
-                            null,
-                            "Incorrect credentials or access rights.",
-                            null),
+                    new SearchResultDoneProtocolOp(ResultCode.INVALID_CREDENTIALS_INT_VALUE, null,
+                            "Incorrect credentials or access rights.", null),
                     Collections.emptyList());
         }
 
