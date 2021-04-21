3f7079e670769b0a0800e034f44a420b4551cfc4
Bondarenko Dmitry <dmitry.bondarenko@wilix.org>
2021-04-21 13:23:22 +0300

    Добавлен файл json с информацией о группах и пользователях, под него перестроена логика получения этой информации.

--- server/src/test/java/dev/wilix/ldap/facade/server/ServerTest.java
+++ server/src/test/java/dev/wilix/ldap/facade/server/ServerTest.java
@@ -34,26 +34,21 @@ public class ServerTest {
     private final String[] ALL_USERS_ATTRIBUTES = {"company", "id", "entryuuid", "uid", "cn", "gn", "sn", "active", "telephoneNumber", "mail", "vcsName", "memberof"};
     private final String[] ALL_GROUPS_ATTRIBUTES = {"id", "primarygrouptoken", "gidnumber", "entryuuid", "uid", "cn", "member"};
 
-    private BindResult bindResult;
-    private SearchResult search;
-
     @Test
-    public void positiveUserAuthenticate() {
+    public void positiveUserAuthenticate() throws LDAPException {
+        BindResult bindResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
         }
 
         LDAPTestUtils.assertResultCodeEquals(bindResult, ResultCode.SUCCESS);
     }
 
     @Test
-    public void positiveServiceAuthenticate() {
+    public void positiveServiceAuthenticate() throws LDAPException {
+        BindResult bindResult;
         try (LDAPConnection ldap = openLDAP()) {
-            serviceBindResult(ldap);
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = serviceBindResult(ldap);
         }
 
         LDAPTestUtils.assertResultCodeEquals(bindResult, ResultCode.SUCCESS);
@@ -63,7 +58,7 @@ public class ServerTest {
     public void negativeUserAuthenticate() {
         assertThrows(LDAPException.class, () -> {
             try (LDAPConnection ldap = openLDAP()) {
-                userBindResult(ldap, "wrongUsername", "wrongPassword");
+                BindResult bindResult = userBindResult(ldap, "wrongUsername", "wrongPassword");
             }
         });
     }
@@ -72,173 +67,173 @@ public class ServerTest {
     public void negativeServiceAuthenticate() {
         assertThrows(LDAPException.class, () -> {
             try (LDAPConnection ldap = openLDAP()) {
-                serviceBindResult(ldap, "wrongServiceName", "wrongToken");
+                BindResult bindResult = serviceBindResult(ldap, "wrongServiceName", "wrongToken");
             }
         });
     }
 
     @Test
-    public void searchAllEntityFromRootDirectory() {
+    public void searchAllEntityFromRootDirectory() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search("dc=example,dc=com", SearchScope.SUB, "(uid=*)");
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search("dc=example,dc=com", SearchScope.SUB, "(uid=*)");
         }
 
-        checkResultsOfRequest(4);
+        checkResultsOfRequest(4, bindResult, searchResult);
     }
 
     @Test
-    public void searchAllEntityFromUsersDirectory() {
+    public void searchAllEntityFromUsersDirectory() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search(USER_BASE_DN, SearchScope.SUB, "(uid=*)");
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search(USER_BASE_DN, SearchScope.SUB, "(uid=*)");
         }
 
-        checkResultsOfRequest(2);
+        checkResultsOfRequest(2, bindResult, searchResult);
     }
 
     @Test
-    public void searchAllEntityFromGroupsDirectory() {
+    public void searchAllEntityFromGroupsDirectory() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search(GROUP_BASE_DN, SearchScope.SUB, "(uid=*)");
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search(GROUP_BASE_DN, SearchScope.SUB, "(uid=*)");
         }
 
-        checkResultsOfRequest(2);
+        checkResultsOfRequest(2, bindResult, searchResult);
     }
 
     @Test
-    public void searchUserWhoParticipateInOneGroup() {
+    public void searchUserWhoParticipateInOneGroup() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(memberof=uid=groupTwo,ou=groups,dc=example,dc=com)"));
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(memberof=uid=groupTwo,ou=groups,dc=example,dc=com)"));
         }
 
-        checkResultsOfRequest(1);
+        checkResultsOfRequest(1, bindResult, searchResult);
     }
 
     @Test
-    public void searchUserByPattern() {
+    public void searchUserByPattern() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(uid=u*e)"));
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(uid=u*e)"));
         }
 
-        checkResultsOfRequest(1);
+        checkResultsOfRequest(1, bindResult, searchResult);
     }
 
     @Test
-    public void searchUserWhoParticipateInAllTwoGroup() {
+    public void searchUserWhoParticipateInAllTwoGroup() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(&(memberof=uid=groupOne,ou=groups,dc=example,dc=com)(memberof=uid=groupTwo,ou=groups,dc=example,dc=com))"));
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(&(memberof=uid=groupOne,ou=groups,dc=example,dc=com)(memberof=uid=groupTwo,ou=groups,dc=example,dc=com))"));
         }
 
-        checkResultsOfRequest(1);
+        checkResultsOfRequest(1, bindResult, searchResult);
     }
 
     @Test
-    public void searchUserWhoIsNotActive() {
+    public void searchUserWhoIsNotActive() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(active=false)"));
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(active=false)"));
         }
 
-        checkResultsOfRequest(1);
+        checkResultsOfRequest(1, bindResult, searchResult);
     }
 
     @Test
-    public void searchUserWithNotInitializeAttribute() {
+    public void searchUserWithNotInitializeAttribute() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(!(notExistAttribute=*))"));
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search(USER_BASE_DN, SearchScope.SUB, ("(!(notExistAttribute=*))"));
         }
 
-        checkResultsOfRequest(2);
+        checkResultsOfRequest(2, bindResult, searchResult);
     }
 
     @Test
-    public void searchUsersEntityFromRootDirectoryByTheirClassName() {
+    public void searchUsersEntityFromRootDirectoryByTheirClassName() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search("dc=example,dc=com", SearchScope.SUB, "(objectClass=organizationalPerson)");
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search("dc=example,dc=com", SearchScope.SUB, "(objectClass=organizationalPerson)");
         }
 
-        checkResultsOfRequest(2);
+        checkResultsOfRequest(2, bindResult, searchResult);
     }
 
     @Test
-    public void searchGroupsEntityFromRootDirectoryByTheirClassName() {
+    public void searchGroupsEntityFromRootDirectoryByTheirClassName() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search("dc=example,dc=com", SearchScope.SUB, "(objectClass=groupOfNames)");
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search("dc=example,dc=com", SearchScope.SUB, "(objectClass=groupOfNames)");
         }
 
-        checkResultsOfRequest(2);
+        checkResultsOfRequest(2, bindResult, searchResult);
     }
 
     @Test
-    public void searchGroupsEntityByCompleteMatcherTheirDn() {
+    public void searchGroupsEntityByCompleteMatcherTheirDn() throws LDAPException {
+        BindResult bindResult;
+        SearchResult searchResult;
         try (LDAPConnection ldap = openLDAP()) {
-            userBindResult(ldap);
-            search = ldap.search("dc=example,dc=com", SearchScope.SUB, "(dn=uid=groupOne,ou=groups,dc=example,dc=com)");
-        } catch (LDAPException e) {
-            e.printStackTrace();
+            bindResult = userBindResult(ldap);
+            searchResult = ldap.search("dc=example,dc=com", SearchScope.SUB, "(dn=uid=groupOne,ou=groups,dc=example,dc=com)");
         }
 
-        checkResultsOfRequest(1);
+        checkResultsOfRequest(1, bindResult, searchResult);
     }
 
     private LDAPConnection openLDAP() throws LDAPException {
         return new LDAPConnection("localhost", 636);
     }
 
-    private void userBindResult(LDAPConnection ldap) throws LDAPException {
+    private BindResult userBindResult(LDAPConnection ldap) throws LDAPException {
         String usernameForLdap = String.format("uid=%s,%s", USERNAME, USER_BASE_DN);
-        bindResult = ldap.bind(usernameForLdap, USER_PASSWORD);
+        return ldap.bind(usernameForLdap, USER_PASSWORD);
     }
 
-    private void userBindResult(LDAPConnection ldap, String username, String password) throws LDAPException {
+    private BindResult userBindResult(LDAPConnection ldap, String username, String password) throws LDAPException {
         String usernameForLdap = String.format("uid=%s,%s", username, USER_BASE_DN);
-        bindResult = ldap.bind(usernameForLdap, password);
+        return ldap.bind(usernameForLdap, password);
     }
 
-    private void serviceBindResult(LDAPConnection ldap) throws LDAPException {
+    private BindResult serviceBindResult(LDAPConnection ldap) throws LDAPException {
         String serviceNameForLdap = String.format("uid=%s,%s", SERVICE_NAME, SERVICE_BASE_DN);
-        bindResult = ldap.bind(serviceNameForLdap, SERVICE_TOKEN);
+        return ldap.bind(serviceNameForLdap, SERVICE_TOKEN);
     }
 
-    private void serviceBindResult(LDAPConnection ldap, String service, String token) throws LDAPException {
+    private BindResult serviceBindResult(LDAPConnection ldap, String service, String token) throws LDAPException {
         String serviceNameForLdap = String.format("uid=%s,%s", service, SERVICE_BASE_DN);
-        bindResult = ldap.bind(serviceNameForLdap, token);
+        return ldap.bind(serviceNameForLdap, token);
     }
 
-    private void checkResultsOfRequest(int numberOfExpectReceiveEntries) {
+    private void checkResultsOfRequest(int numberOfExpectReceiveEntries, BindResult bindResult, SearchResult searchResult) {
         LDAPTestUtils.assertResultCodeEquals(bindResult, ResultCode.SUCCESS);
-        LDAPTestUtils.assertResultCodeEquals(search, ResultCode.SUCCESS);
-        LDAPTestUtils.assertEntriesReturnedEquals(search, numberOfExpectReceiveEntries);
+        LDAPTestUtils.assertResultCodeEquals(searchResult, ResultCode.SUCCESS);
+        LDAPTestUtils.assertEntriesReturnedEquals(searchResult, numberOfExpectReceiveEntries);
 
-        search.getSearchEntries().forEach(searchEntry -> selectSuitableMethodCheckSearchResultAttributes(searchEntry, searchEntry.getAttribute("objectClass").getValue()));
+        searchResult.getSearchEntries().forEach(searchEntry -> selectSuitableMethodCheckSearchResultAttributes(searchEntry, searchEntry.getAttribute("objectClass").getValue()));
     }
 
     private void selectSuitableMethodCheckSearchResultAttributes(SearchResultEntry entry, String objectClass) {
--- server/src/test/java/dev/wilix/ldap/facade/server/TestStorage.java
+++ server/src/test/java/dev/wilix/ldap/facade/server/TestStorage.java
@@ -1,33 +1,32 @@
 package dev.wilix.ldap.facade.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.wilix.ldap.facade.api.Authentication;
-import org.springframework.boot.SpringApplication;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.*;
 
 public class TestStorage implements dev.wilix.ldap.facade.api.DataStorage {
+
     private List<Map<String, List<String>>> users = null;
     private List<Map<String, List<String>>> groups = null;
-    private final String userName = "username";
-    private final String userPassword = "password";
-    private final String nameOfService = "serviceName";
-    private final String tokenOfService = "token";
-    private final String[] ALL_USERS_ATTRIBUTES = {"company", "id", "entryuuid", "uid", "cn", "gn", "sn", "active", "telephoneNumber", "mail", "memberof", "vcsName"};
-    private final String[] ALL_GROUPS_ATTRIBUTES = {"id", "primarygrouptoken", "gidnumber", "entryuuid", "uid", "cn", "member"};
-    private final List<List<String>> FIRST_USER_VALUES = Arrays.asList(List.of("Wilix"), List.of("1"), List.of("1"), List.of("username"), List.of("FirstName SecondName"), List.of("FirstName"), List.of("SecondName"), List.of("true"), List.of("88005553535"), List.of("email@e.mail"), List.of("groupOne", "groupTwo"), List.of("FirstName SecondName", "88005553535"));
-    private final List<List<String>> SECOND_USER_VALUES = Arrays.asList(List.of("Wilix"), List.of("2"), List.of("2"), List.of("username2"), List.of("FirstName2 SecondName2"), List.of("FirstName2"), List.of("SecondName2"), List.of("false"), List.of("12345678900"), List.of("email2@e.mail"), List.of("groupOne"), List.of("FirstName2 SecondName2", "12345678900"));
-    private final List<List<String>> FIRST_GROUP_VALUES = Arrays.asList(List.of("1"), List.of("1"), List.of("1"), List.of("1"), List.of("groupOne"), List.of("groupOne"), List.of("username", "username2"));
-    private final List<List<String>> SECOND_GROUP_VALUES = Arrays.asList(List.of("1"), List.of("2"), List.of("2"), List.of("2"), List.of("groupTwo"), List.of("groupTwo"), List.of("username"));
 
-    public static void main(String[] args) {
-        SpringApplication.run(TestStorage.class, args);
-    }
+    private final Path PATH_TO_FILE = Path.of("./src/test/resources/users_and_groups_file.json");
+
+    private final String USERNAME = "username";
+    private final String USER_PASSWORD = "password";
+    private final String NAME_OF_SERVICE = "serviceName";
+    private final String TOKEN_OF_SERVICE = "token";
 
     @PostConstruct
-    private void prepareEntriesInfo() {
-        users = addUsers();
-        groups = addGroups();
+    private void prepareEntriesInfo() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, List<Map<String, List<String>>>> usersAndGroupsInfo = objectMapper.readValue(PATH_TO_FILE.toFile(), Map.class);
+        users = usersAndGroupsInfo.get("users");
+        groups = usersAndGroupsInfo.get("groups");
     }
 
     @Override
@@ -42,28 +41,11 @@ public class TestStorage implements dev.wilix.ldap.facade.api.DataStorage {
 
     @Override
     public Authentication authenticateUser(String username, String password) {
-        return (username.equals(userName) && password.equals(userPassword)) ? Authentication.POSITIVE : Authentication.NEGATIVE;
+        return (username.equals(USERNAME) && password.equals(USER_PASSWORD)) ? Authentication.POSITIVE : Authentication.NEGATIVE;
     }
 
     @Override
     public Authentication authenticateService(String serviceName, String token) {
-        return (serviceName.equals(nameOfService) && token.equals(tokenOfService)) ? Authentication.POSITIVE : Authentication.NEGATIVE;
+        return (serviceName.equals(NAME_OF_SERVICE) && token.equals(TOKEN_OF_SERVICE)) ? Authentication.POSITIVE : Authentication.NEGATIVE;
     }
-
-    private List<Map<String, List<String>>> addUsers() {
-        return List.of(addInfoToMap(ALL_USERS_ATTRIBUTES, FIRST_USER_VALUES), addInfoToMap(ALL_USERS_ATTRIBUTES, SECOND_USER_VALUES));
-    }
-
-    private List<Map<String, List<String>>> addGroups() {
-        return List.of(addInfoToMap(ALL_GROUPS_ATTRIBUTES, FIRST_GROUP_VALUES), addInfoToMap(ALL_GROUPS_ATTRIBUTES, SECOND_GROUP_VALUES));
-    }
-
-    private Map<String, List<String>> addInfoToMap(String[] attributesNames, List<List<String>> attributesValues) {
-        Map<String, List<String>> infoOfEntry = new HashMap<>();
-        for (int i = 0; i < attributesNames.length; i++) {
-            infoOfEntry.put(attributesNames[i], attributesValues.get(i));
-        }
-        return infoOfEntry;
-    }
-
 }
